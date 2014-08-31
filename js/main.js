32c3f7319796e9b5fb32a9e5a6f51b66fb6d27a5
Cara <cwarner@opensky.com>
2014-08-31 19:57:12 -0400

    Basic parallax working using skrollr

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-;(function() {
-    console.log("Hello world!");
-}());
+( function( $ ) {
+
+    // Setup variables
+    $window = $(window);
+    $parallax = $('.parallax');
+    $body = $('body');
+
+    $body.imagesLoaded( function() {
+        setTimeout(function() {
+            console.log("Images loaded");
+            // Resize sections
+            adjustWindow();
+
+        }, 800);
+    });
+
+    function adjustWindow(){
+        console.log("Adjusting window");
+        // Init Skrollr
+        var s = skrollr.init({
+            forceHeight: false
+        });
+
+        // Get window size
+        winH = $window.height();
+
+        // Keep minimum height 550
+        if(winH <= 550) {
+            winH = 550;
+        }
+
+        // Resize our slides
+        $parallax.height(winH);
+
+        // Refresh Skrollr after resizing our sections
+        s.refresh($('.parallax'));
+    }
+
+} )( jQuery );
