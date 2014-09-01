e577e7f42662c421cff480179dad30e9af78d9c0
Cara <cwarner@opensky.com>
2014-08-31 21:34:26 -0400

    Cleaned up images

--- js/main.js
+++ js/main.js
@@ -5,17 +5,10 @@
     $parallax = $('.parallax');
     $body = $('body');
 
-    $body.imagesLoaded( function() {
-        setTimeout(function() {
-            console.log("Images loaded");
-            // Resize sections
-            adjustWindow();
-
-        }, 800);
-    });
+    adjustWindow();
 
     function adjustWindow(){
-        console.log("Adjusting window");
+
         // Init Skrollr
         var s = skrollr.init({
             forceHeight: false
@@ -24,12 +17,7 @@
         // Get window size
         winH = $window.height();
 
-        // Keep minimum height 550
-        if(winH <= 550) {
-            winH = 550;
-        }
-
-        // Resize our slides
+        // Resize slides
         $parallax.height(winH);
 
         // Refresh Skrollr after resizing our sections
