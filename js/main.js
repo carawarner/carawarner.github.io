896646685a960511eb7ecb06e07487591ab4b6a8
Cara <cwarner@opensky.com>
2014-09-16 08:19:55 -0400

    Removing js parallax, using CSS only for smoother experience

--- js/main.js
+++ js/main.js
@@ -2,28 +2,6 @@
 
     // Setup variables
     $window = $(window);
-    $parallax = $('.fill-port');
     $body = $('body');
-
-    adjustWindow();
-
-    $(".sticky-heading").sticky({topSpacing:0});
-
-    function adjustWindow(){
-
-        // Init Skrollr
-        var s = skrollr.init({
-            forceHeight: false
-        });
-
-        // Get window size
-        winH = $window.height();
-
-        // Resize slides
-        $parallax.height(winH);
-
-        // Refresh Skrollr after resizing our sections
-        s.refresh($('.parallax'));
-    }
 
 } )( jQuery );
