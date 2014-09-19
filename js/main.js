4b5c671773dfc5b2fff324b867897ce66be7b758
Cara <cwarner@opensky.com>
2014-09-19 07:38:17 -0400

    Hide bg image until it has fully loaded

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@
     $window = $(window);
     $body = $('body');
 
+    var img = new Image();
+    img.src = 'http://carawarner.github.io/img/cloudy-landscape.jpg';
+
+    console.log("hello");
+    var int = setInterval(function() {
+        console.log("This is firing");
+        if (img.complete) {
+            clearInterval(int);
+            document.getElementById('hello').style.backgroundImage = 'url(' + img.src + ')';
+        }
+    }, 50);
+
 } )( jQuery );
