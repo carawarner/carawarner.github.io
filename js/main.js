5989b77b5a93ff5652aafd813d7fb364ac4b3ec6
Cara <cwarner@opensky.com>
2014-09-20 21:50:45 -0400

    Cleaned up some styling, made sure it looks good in mobile aspect

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 ( function( $ ) {
 
-    // Setup variables
-    $window = $(window);
-    $body = $('body');
 
+    // Don't display image until it has loaded fully
     var img = new Image();
     img.src = 'http://carawarner.github.io/img/cloudy-landscape.jpg';
 
-    console.log("hello");
     var int = setInterval(function() {
-        console.log("This is firing");
         if (img.complete) {
             clearInterval(int);
             document.getElementById('hello').style.backgroundImage = 'url(' + img.src + ')';
         }
     }, 50);
+
+    // Alter nav bar to preserve legibility when moving from dark bg to light
+
+    var mainbottom = $('#hello').offset().top + $('#hello').height() - $('#navigation').height();
+    console.log(mainbottom);
+
+    $(window).scroll(function(){
+        console.log("testing");
+    });
+
+    // on scroll,
+    $(window).on('scroll',function(){
+        console.log("scrolling");
+        // we round here to reduce a little workload
+        stop = Math.round($(window).scrollTop());
+        if (stop > mainbottom) {
+            console.log("invert");
+            $('#navigation').addClass('inverted');
+        } else {
+            console.log("uninvert");
+            $('#navigation').removeClass('inverted');
+        }
+
+    });
+
 
 } )( jQuery );
