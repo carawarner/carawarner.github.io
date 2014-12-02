e2690974e261a95f20818ace377c760fad06372c
Cara <cwarner@opensky.com>
2014-12-01 22:54:44 -0500

    Remove deprecated code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 ( function( $ ) {
 
-
     // Don't display image until it has loaded fully
     var img = new Image();
     img.src = 'http://carawarner.github.io/img/cloudy-landscape.jpg';
@@ -11,30 +10,5 @@
             document.getElementById('hello').style.backgroundImage = 'url(' + img.src + ')';
         }
     }, 50);
-
-    // Alter nav bar to preserve legibility when moving from dark bg to light
-
-    var mainbottom = $('#hello').offset().top + $('#hello').height() - $('#navigation').height();
-    console.log(mainbottom);
-
-    $(window).scroll(function(){
-        console.log("testing");
-    });
-
-    // on scroll,
-    $(window).on('scroll',function(){
-        console.log("scrolling");
-        // we round here to reduce a little workload
-        stop = Math.round($(window).scrollTop());
-        if (stop > mainbottom) {
-            console.log("invert");
-            $('#navigation').addClass('inverted');
-        } else {
-            console.log("uninvert");
-            $('#navigation').removeClass('inverted');
-        }
-
-    });
-
 
 } )( jQuery );
