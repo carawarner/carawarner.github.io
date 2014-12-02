2aed25ea296bf02cd3ab531f0ffd02deee0bfd46
Cara <cwarner@opensky.com>
2014-12-01 23:56:26 -0500

    Welcome back, parallax effect

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 ( function( $ ) {
 
-    // Don't display image until it has loaded fully
-    var img = new Image();
-    img.src = 'http://carawarner.github.io/img/cloudy-landscape.jpg';
-
-    var int = setInterval(function() {
-        if (img.complete) {
-            clearInterval(int);
-            document.getElementById('hello').style.backgroundImage = 'url(' + img.src + ')';
-        }
-    }, 50);
+    /** Parallax effect */
+    var s = skrollr.init();
+    s.refresh($('.parallax'));
 
 } )( jQuery );
